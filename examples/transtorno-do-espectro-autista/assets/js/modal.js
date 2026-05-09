1cec9b66390338aaa4028cc0c2804da429c31aea
Erme Schultz <ermelindo.schultz@gmail.com>
2026-05-09 12:03:06 -0300

    docs: complement examples folder

---
+++ examples/transtorno-do-espectro-autista/assets/js/modal.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var stopVideos = function () {
+    var videos = document.querySelectorAll('iframe, video');
+    Array.prototype.forEach.call(videos, function (video) {
+      if (video.tagName.toLowerCase() === 'video') {
+        video.pause();
+      } else {
+        var src = video.src;
+        video.src = src;
+      }
+    });
+  };
+
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+    stopVideos();
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // abrir modal sem numero com const
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const image = $trigger.querySelector('img');
+    const imagePath = image.getAttribute('src');
+    const $target = document.getElementById('modal-js-example');
+    
+    $trigger.addEventListener('click', () => {
+      const $targetImage = $target.querySelector('img');
+      $targetImage.setAttribute('src', imagePath);
+      openModal($target);
+    });
+  });
+
+  // abrir modal numerado
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
+
+function toggleVideo(target) {
+  document.querySelector(target).classList.toggle('video-open')
+}
